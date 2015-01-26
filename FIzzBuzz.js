70b51220355d1fcaf5a84af5e147c9010a146adf
Connor Ring <crv9557@saintjoe.edu>
2015-01-26 12:16:18 -0500

    Added comments

--- FIzzBuzz.js
+++ FIzzBuzz.js
@@ -1,4 +1,8 @@
+//Connor Ring
+//CMP237 Chapter 2 Exercise
+//Last worked on: 1/22/2015
 // Prints out #1-100, changing numbers divisible by 3 and 5
+
  for (var n = 1; n <= 100; n++) {
    output = "";
   if (n % 3 == 0)
